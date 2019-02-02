4245f58cd771397c93ee12e6cf6b9a5a3be08964
Max Davis <mdavis@Maxs-MacBook-Pro.local>
2019-02-02 15:32:03 -0800

    initial commit.

---
+++ BarbellListener WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BarbellListener WatchKit Extension
+//
+//  Created by Max Davis on 2/2/19.
+//  Copyright © 2019 Max Davis. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
